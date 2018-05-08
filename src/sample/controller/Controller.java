944fda260b9f1a51034ee1e5ebbdf4350a8b3ad3
AhmedKhaledAK <ahmedkhaledabab@gmail.com>
2018-05-08 17:05:21 +0200

    Bombs again
    Generating random bombs, editing in the 2d array, re-drawing the array after a bomb explosion

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -1,3 +1,4 @@
+
 package sample.controller;
 
 import javafx.fxml.FXML;
@@ -23,180 +24,173 @@ public class Controller {
     @FXML
     Label lblMoves, lblHealth;
 
-    InfoPanel infoPanel = InfoPanel.getInstance();
-    Runner runnerObject = Runner.getInstance();
+    private InfoPanel infoPanel = InfoPanel.getInstance();
+    private Runner runnerObject = Runner.getInstance();
 
     private FileClass fileClass = new FileClass();
-    private char [][] gameMap = fileClass.getMaze();
-    private int row=29, col=0;
+    private char[][] gameMap = fileClass.getMaze();
+    private int row = 29, col = 0;
 
     private ImageView runner;
-    Item item;
-    ItemFactory itemFactory = new ItemFactory();
+    private Item item;
+    private ItemFactory itemFactory = new ItemFactory();
 
-    @FXML
-    public void initialize()
-    {
-        try{
-            Image image = null;
+    private void setUpArray(){
+        try {
+            Image image;
 
-            for(int i=0; i<30; i++)
-            {
-                for(int j=0; j<30; j++)
-                {
-                    if(gameMap[i][j] == 'W')
-                    {
+            for (int i = 0; i < 30; i++) {
+                for (int j = 0; j < 30; j++) {
+                    if (gameMap[i][j] == 'W') {
                         image = new Image(new FileInputStream("res/Photos/wall.png"));
                         System.out.printf("at [%d][%d], %c found\n", i, j, gameMap[i][j]);
-                    }
-                    else if(gameMap[i][j] == 'E')
-                    {
+                    } else if (gameMap[i][j] == 'E') {
                         image = new Image(new FileInputStream("res/Photos/empty.png"));
                         System.out.printf("at [%d][%d], %c found\n", i, j, gameMap[i][j]);
-
-                    }
-                    else if(gameMap[i][j] == 'B' || gameMap[i][j] == 'D')
-                    {
+                    } else {
                         item = itemFactory.createItem(gameMap[i][j]);
                         image = item.getImage();
+                        gameMap[i][j] = itemFactory.getX();
                     }
 
                     ImageView img = new ImageView(image);
 
-                    img.setLayoutX(j*20);
-                    img.setLayoutY(i*20);
+                    img.setLayoutX(j * 20);
+                    img.setLayoutY(i * 20);
 
                     pane.getChildren().add(img);
 
                 }
             }
 
-            image = new Image(new FileInputStream("res/Photos/runner.png"));
-
-            runner = new ImageView(image);
-
-            runner.setLayoutX(0);
-            runner.setLayoutY(29*20);
-
-            pane.getChildren().add(runner);
-
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private void setUpGame(){
+        try {
+            Image image;
+
+            setUpArray();
+
+            image = new Image(new FileInputStream("res/Photos/runner.png"));
+
+            runner = new ImageView(image);
+            runner.setLayoutX(0);
+            runner.setLayoutY(29 * 20);
+
+            pane.getChildren().add(runner);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        setUpGame();
+    }
 
     public void onKeyPressed(KeyEvent keyEvent) {
-        if(keyEvent.getCode() == KeyCode.D){
-            if(gameMap[row][col+1] == 'E'){
+        if (keyEvent.getCode() == KeyCode.D) {
+            if (gameMap[row][col + 1] != 'E' && gameMap[row][col + 1] != 'W') {
+                col++;
+                item = itemFactory.createItem(gameMap[row][col]);
+                gameMap[row][col] = 'E';
+                runner.setLayoutX(runner.getLayoutX() + 20);
+                updateLives();
+                updateMoves();
+
+                pane.getChildren().remove(runner);
+                setUpArray();
+                pane.getChildren().add(runner);
+            } else if (gameMap[row][col + 1] == 'E') {
                 System.out.println("You can move here");
                 col++;
                 runner.setLayoutX(runner.getLayoutX() + 20);
-
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-            }
-            else if(gameMap[row][col+1] == 'B' || gameMap[row][col+1] == 'D'){
-                col++;
-                runner.setLayoutX(runner.getLayoutX() + 20);
-
-                item.change(runnerObject);
-                lblHealth.setText(Integer.toString(infoPanel.getLives()));
-                System.out.println("FOUND A BOMB");
-
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-
-            }
-            else {
-                System.out.println("You cannot move here");
+                updateMoves();
             }
         }
-        else if(keyEvent.getCode() == KeyCode.A){
-            if(gameMap[row][col-1] == 'E'){
+
+        else if (keyEvent.getCode() == KeyCode.A) {
+            if (gameMap[row][col - 1] != 'E' && gameMap[row][col - 1] != 'W') {
+                col--;
+                item = itemFactory.createItem(gameMap[row][col]);
+                gameMap[row][col]= 'E';
+                runner.setLayoutX(runner.getLayoutX() - 20);
+                updateLives();
+                updateMoves();
+
+                pane.getChildren().remove(runner);
+                setUpArray();
+                pane.getChildren().add(runner);
+            } else if (gameMap[row][col - 1] == 'E') {
                 System.out.println("You can move here");
                 col--;
                 runner.setLayoutX(runner.getLayoutX() - 20);
 
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-
-            }
-            else if(gameMap[row][col-1] == 'B' || gameMap[row][col-1] == 'D'){
-                col--;
-                runner.setLayoutX(runner.getLayoutX() - 20);
-
-                item.change(runnerObject);
-                lblHealth.setText(Integer.toString(infoPanel.getLives()));
-                System.out.println("FOUND A BOMB");
-
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-
-            }
-            else {
-                System.out.println("You cannot move here");
+                updateMoves();
             }
         }
-        else if(keyEvent.getCode() == KeyCode.W){
-            if(gameMap[row-1][col] == 'E'){
+
+        else if (keyEvent.getCode() == KeyCode.W) {
+            if (gameMap[row - 1][col] != 'E' && gameMap[row - 1][col] != 'W') {
+                row--;
+                item = itemFactory.createItem(gameMap[row][col]);
+                gameMap[row][col] = 'E';
+                runner.setLayoutY(runner.getLayoutY() - 20);
+                updateLives();
+                updateMoves();
+                setUpArray();
+
+
+                pane.getChildren().remove(runner);
+                setUpArray();
+                pane.getChildren().add(runner);
+            } else if (gameMap[row - 1][col] == 'E') {
                 System.out.println("You can move here");
                 row--;
                 runner.setLayoutY(runner.getLayoutY() - 20);
 
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-            }
-            else if(gameMap[row-1][col] == 'B' || gameMap[row-1][col] == 'D'){
-                row--;
-                runner.setLayoutY(runner.getLayoutY() - 20);
-
-                item.change(runnerObject);
-                lblHealth.setText(Integer.toString(infoPanel.getLives()));
-                System.out.println("FOUND A BOMB");
-
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-
-            }
-            else {
-                System.out.println("You cannot move here");
+                updateMoves();
             }
         }
-        else if(keyEvent.getCode() == KeyCode.S){
-            if(gameMap[row+1][col] == 'E'){
+
+        else if (keyEvent.getCode() == KeyCode.S) {
+            if (gameMap[row + 1][col] != 'E' && gameMap[row + 1][col] != 'W') {
+                row++;
+                item = itemFactory.createItem(gameMap[row][col]);
+                gameMap[row][col] = 'E';
+                runner.setLayoutY(runner.getLayoutY() + 20);
+                updateLives();
+                updateMoves();
+                setUpArray();
+
+
+                pane.getChildren().remove(runner);
+                setUpArray();
+                pane.getChildren().add(runner);
+            } else if (gameMap[row + 1][col] == 'E') {
                 System.out.println("You can move here");
                 row++;
                 runner.setLayoutY(runner.getLayoutY() + 20);
 
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-            }
-            else if(gameMap[row+1][col] == 'B' || gameMap[row+1][col] == 'D'){
-                row++;
-                runner.setLayoutY(runner.getLayoutY() + 20);
-
-                item.change(runnerObject);
-                lblHealth.setText(Integer.toString(infoPanel.getLives()));
-                System.out.println("FOUND A BOMB");
-
-                runnerObject.setMoves(runnerObject.getMoves() + 1);
-                lblMoves.setText(Integer.toString(infoPanel.getMoves()));
-                System.out.println(infoPanel.getMoves());
-
-            }
-            else {
-                System.out.println("You cannot move here");
+                updateMoves();
             }
         }
+    }
+
+    private void updateMoves() {
+        runnerObject.setMoves(runnerObject.getMoves() + 1);
+        lblMoves.setText(Integer.toString(infoPanel.getMoves()));
+        System.out.println(infoPanel.getMoves());
+    }
+
+    private void updateLives() {
+        item.change(runnerObject);
+        lblHealth.setText(Integer.toString(infoPanel.getLives()));
+        System.out.println("FOUND A BOMB");
     }
 
 }
--- src/sample/model/items/ItemFactory.java
+++ src/sample/model/items/ItemFactory.java
@@ -3,15 +3,39 @@ package sample.model.items;
 import sample.model.items.Bombs.LivesBomb;
 import sample.model.items.Bombs.LivesCoinsBomb;
 
+import java.util.Random;
+
 public class ItemFactory {
 
-    public Item createItem(char item){
-        switch (item){
+    private char x;
+
+    public Item createItem(char item) {
+        switch (item) {
             case 'B':
+                x='B';
                 return new LivesBomb();
             case 'D':
+                x='D';
+                return new LivesCoinsBomb();
+            default:
+                return selectItem();
+        }
+    }
+
+    private Item selectItem() {
+        int randomNumber = new Random().nextInt(2) + 1;
+        switch (randomNumber) {
+            case 1:
+                x='B';
+                return new LivesBomb();
+            case 2:
+                x='D';
                 return new LivesCoinsBomb();
         }
         return null;
+    }
+
+    public char getX() {
+        return x;
     }
 }
